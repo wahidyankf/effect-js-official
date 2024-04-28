dd8bc88d856535718bdeffeb94b2c566000a9b5a
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2024-04-28 16:52:25 +0700

    hello effect

--- main.ts
+++ main.ts
@@ -1,8 +1,5 @@
-export function add(a: number, b: number): number {
-  return a + b;
-}
+import { Effect, Console } from "npm:effect";
 
-// Learn more at https://deno.land/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
-}
+const program = Console.log("Hello, World!");
+
+Effect.runSync(program);
